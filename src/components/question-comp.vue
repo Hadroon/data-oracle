68b9eee5182b58ad98e1ee383446f1bc89240aba
gabormuranyi-planorama <gabor.muranyi@planorama.com>
2019-01-04 17:49:52 +0100

    upload

---
+++ src/components/question-comp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+      Hello from mainView :)
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'question-comp'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <questionComp />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import questionComp from '@/components/question-comp.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    questionComp
   }
 }
 </script>
+
+<style>
+
+/* xs, extra-small: 0px or larger
+sm, small: 600px or larger
+md, medium: 960px or larger
+lg, large: 1280px or larger
+xl, extra-large: 1920px or larger */
+
+</style>
