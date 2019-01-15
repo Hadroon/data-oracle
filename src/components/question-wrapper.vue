c5f88047c34f3f837abe2c8ddaf674dd26db4382
gabormuranyi-planorama <gabor.muranyi@planorama.com>
2019-01-15 16:51:49 +0100

    upload

---
+++ src/components/question-wrapper.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="question-wrapper" :style="{'width': maxWidth }">
+  <div class="float-left" :style="{'width': windowWidth + 'px',
+   'background-color': red,
+   'height': windowHeight + 'px' }" >
+    <p>{{position}}</p>
+    <button @click="turnPage">go to second</button>
+   </div>
+  <div class="float-left" :style="{'width': windowWidth + 'px',
+   'background-color': green,
+   'height': windowHeight }" >
+    <button @click="turnPage">go to third</button>
+   </div>
+  <div class="float-left" :style="{'width': windowWidth + 'px',
+   'background-color': yellow,
+   'height': windowHeight }" >third</div>
+
+</div>
+</template>
+
+<script>
+
+// get the datas from store
+// store answers
+// when answer a question animate to the next one
+// when finshed send to the server
+
+export default {
+  name: 'question-comp',
+  data () {
+    return {
+      windowWidth: window.innerWidth,
+      windowHeight: window.innerHeight,
+      red: 'red',
+      green: 'green',
+      yellow: 'yellow',
+      ani: null,
+      fontSize: '24px',
+      position: 0
+    }
+  },
+  computed: {
+    innerWidth: function () {
+      let width = window.innerWidth / 2
+      return width
+    },
+    maxWidth: function () {
+      return (this.windowWidth * 4) + 'px'
+    }
+  },
+  methods: {
+    out: function (e) {
+      e.preventDefault()
+      this.ani = {
+        animated: true,
+        bounceOut: true
+      }
+    },
+    turnPage: function () {
+      this.position = this.position - windowWidth
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+@media only screen and (max-width: 600px) {
+
+.question-wrapper {
+  position: relative;
+}
+
+.float-left {
+  float: left;
+}
+
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <questionComp />
+  <div class="home" :style="{width: newWidth}">
+    <questionWrapper />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import questionComp from '@/components/question-comp.vue'
+import questionWrapper from '@/components/question-wrapper.vue'
 
 export default {
   name: 'home',
   components: {
-    questionComp
+    questionWrapper
+  },
+  data () {
+    return {
+      newWidth: window.innerWidth * 2
+    }
   }
 }
 </script>
@@ -25,5 +28,9 @@ sm, small: 600px or larger
 md, medium: 960px or larger
 lg, large: 1280px or larger
 xl, extra-large: 1920px or larger */
+
+body {
+  margin: 0px;
+}
 
 </style>
