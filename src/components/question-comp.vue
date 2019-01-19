46f4af0a635e365e78eb24c47b0cc5cc5f71434e
Gabor Muranyi <gabor.muranyi@gmail.com>
2019-01-19 22:00:09 +0100

    question and answer design

--- src/components/question-comp.vue
+++ src/components/question-comp.vue
@@ -1,13 +1,14 @@
 <template>
-  <div :style="{'width': windowWidth + 'px', 'height': windowHeight + 'px' }">
-    <div class="questionComp" :style="{'width': windowWidth + 'px', 'height': windowHeight + 'px' }">
-      <div :style="{'background': randomColor()}">
-        <img class="questipnPic" :src="'/img/' + question.picture" alt="">
-      </div>
-      <div :style="{'background': randomColor()}">
+  <div class="questionWrapper" :style="{'width': windowWidth + 'px', 'height': windowHeight + 'px' }">
+    <img class="questipnPic" :src="'/img/' + question.picture" alt="">
+    <div class="content">
+      <h2 :style="{'width': windowWidth * 0.8 + 'px' }">
         {{question.question}}
-        <button @click="turnPage">go to second</button>
-      </div>
+      </h2>
+      <button @click="turnPage" v-for="(answer, index) in question.answers" :key="answer">
+        {{answer}}
+        {{index}}
+      </button>
     </div>
   </div>
 </template>
@@ -50,6 +51,39 @@ export default {
   height: 100%;
   max-height: 100%;
   object-fit: cover;
+}
+
+.content {
+  position: absolute;
+  bottom: 15px;
+  width: 100%;
+}
+
+.questionWrapper {
+  position: relative;
+}
+
+.questionWrapper button {
+  padding: 8px;
+  background-color: rgba(0, 0, 0, 0.5);
+  /* border: 1px solid #ffffff; */
+  border: 0;
+  border-radius: 5px;
+  text-decoration: none;
+  margin: 5px 10px;
+  color: #ffffff;
+  cursor: pointer;
+}
+
+.questionWrapper h2 {
+  display: block;
+  padding: 8px;
+  background-color: rgba(255, 255, 255, 0.5);
+  text-decoration: none;
+  color: #ffffff;
+  width: 100%;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
 }
 
 </style>
